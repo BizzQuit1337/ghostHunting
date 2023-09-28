05339127732cf8391febb1676744c034ca36ddee
Albert Brown <96021456+BizzQuit1337@users.noreply.github.com>
2023-09-28 08:50:53 +0100

    Add files via upload

--- spiriteBox.py
+++ spiriteBox.py
@@ -1,21 +1,26 @@
-from gtts import gTTS
-import os, mmap, random, config
+import mmap, random, pyttsx3
 
 def buildSentance(wordList):
     with open(f"wordsDatabase/{wordList}", "r", encoding="utf-8") as file:
         mmapped_file = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
         lines = mmapped_file.read().decode("utf-8").splitlines()
         mmapped_file.close()
-    sentance = ' '.join([lines[random.randint(0, len(lines) - 1)][:-1] for _ in range(random.randint(1, 10))])
+    sentance = ' '.join([lines[random.randint(0, len(lines) - 1)][:-1] for _ in range(random.randint(1, 5))])
     return sentance
 
-def generateAudio(sentance, fileName):
-    tts = gTTS(text=sentance, lang="en")
-    tts.save(f"audio/{fileName}")
-    os.system(f"start audio/{fileName}")
+def generateAudio(sentance):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[random.randint(0,1)].id)
+    engine.setProperty('volume',random.random()) 
+    engine.setProperty('rate', random.randint(1,200))
+    engine.say(sentance)
+    engine.runAndWait()
 
-counter = 0
+generateAudio(buildSentance('1000WordList.txt'))
 
-while counter > config.limit:
-    generateAudio(buildSentance(config.wordList), config.fileName)
-    counter += 1
+#counter = 0
+
+#while counter > config.limit:
+#    generateAudio(buildSentance(config.wordList), config.fileName)
+#    counter += 1
